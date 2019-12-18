1bc846b3fb9faba95ddb993a1bcd155a2d426fe7
JM0011 <1210652144@qq.com>
2019-12-18 18:28:15 +0800

    finish detail activity

--- app/src/main/java/com/music/fm544/MusicDetailActivity.java
+++ app/src/main/java/com/music/fm544/MusicDetailActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,7 @@ import com.music.fm544.Utils.LocalAudioUtils;
 import com.music.fm544.Utils.StatusBarUtils;
 
 import java.io.File;
+import java.text.DecimalFormat;
 
 public class MusicDetailActivity extends AppCompatActivity {
     private MusicPO mMusic;
@@ -41,7 +43,7 @@ public class MusicDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_music_detail);
 
         mMusic = (MusicPO) getIntent().getSerializableExtra("musicDetail");
-        
+
         songImag = this.findViewById(R.id.song_imag);
         songName = this.findViewById(R.id.song_name);
         singName = this.findViewById(R.id.singer_name);
@@ -52,12 +54,12 @@ public class MusicDetailActivity extends AppCompatActivity {
         musicPath = this.findViewById(R.id.path_value);
 
         backBtn = this.findViewById(R.id.back_btn);
-        okBtn = this.findViewById(R.id.ok_btn);
-        deleteBtn = this.findViewById(R.id.delete_btn);
-        
-        
+//        okBtn = this.findViewById(R.id.ok_btn);
+//        deleteBtn = this.findViewById(R.id.delete_btn);
+
+
         initStatusBar();
-        
+
         initView();
 
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -67,42 +69,56 @@ public class MusicDetailActivity extends AppCompatActivity {
             }
         });
 
-        okBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
+//        okBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                onBackPressed();
+//            }
+//        });
 
-        deleteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                File file = new File("/storage/emulated/0/Music/music/");
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.addCategory(Intent.CATEGORY_DEFAULT);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.setDataAndType(Uri.fromFile(file),"file/*");
-                try {
-//                    startActivity(intent);
-            startActivity(Intent.createChooser(intent,"选择浏览工具"));
-                } catch (ActivityNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+//        deleteBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            }
+//        });
 
     }
 
+    //使用文件管理器打开音乐路径（未实现）
+    private void openExternalFile(MusicPO music) {
+        File file = new File(music.getMusic_path());
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        //判断是否是AndroidN以及更高的版本
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Uri contentUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", file);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+        } else {
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+        }
+     try {
+//        startActivity(intent);
+            startActivity(Intent.createChooser(intent,"选择浏览工具"));
+    } catch(
+    ActivityNotFoundException e)
+    {
+        e.printStackTrace();
+    }
+
+}
+
     private void initView() {
         LocalAudioUtils localAudioUtils = new LocalAudioUtils(this);
-        if (mMusic != null){
+        if (mMusic != null) {
             MusicDetail musicDetail = localAudioUtils.getMusicMessage(mMusic);
-            if (musicDetail != null){
-                if (musicDetail == null || musicDetail.getMusic_pic_path() == null || musicDetail.getMusic_pic_path().equals("")){
+            if (musicDetail != null) {
+                if (musicDetail == null || musicDetail.getMusic_pic_path() == null || musicDetail.getMusic_pic_path().equals("")) {
                     Glide.with(this)
                             .load(R.mipmap.default_music_center)
                             .into(songImag);
-                }else {
+                } else {
                     Glide.with(this)
                             .load(musicDetail.getMusic_pic_path())
                             .into(songImag);
@@ -111,13 +127,51 @@ public class MusicDetailActivity extends AppCompatActivity {
             songName.setText(musicDetail.getMusic_name());
             singName.setText(musicDetail.getMusic_author());
             albumName.setText(musicDetail.getMusic_album());
-            musicSize.setText(musicDetail.getMusic_size().toString());
-            songLength.setText(musicDetail.getMusic_time().toString());
+            String sizeMsg = "";
+            String timeMsg = "";
+            String bitMsg = "";
+            if (musicDetail.getMusic_size() != null && musicDetail.getMusic_size() > 0){
+                DecimalFormat decimalFormat=new DecimalFormat(".00");
+                if (musicDetail.getMusic_size()/1024 > 1){
+                    if ((musicDetail.getMusic_size()/1024)/1024 >1){
+                        double mb = (musicDetail.getMusic_size()/1024.00)/1024.00;
+                        sizeMsg = decimalFormat.format(mb)+"MB";
+                    }else {
+                        double kb = musicDetail.getMusic_size()/1024.00;
+                        sizeMsg = decimalFormat.format(kb)+"KB";
+                    }
+                }else {
+                    sizeMsg = musicDetail.getMusic_size() + "B";
+                }
+            }
+            if (musicDetail.getMusic_time() > 0 && musicDetail.getMusic_time() != null){
+                if (musicDetail.getMusic_time()/1000 > 1){
+                    int s = musicDetail.getMusic_time()/1000;
+                    if (s/60 > 1){
+                        int m = s/60;
+                        if (m/60 > 1 ){
+                            timeMsg = m/60+":"+ m%60 + ":" + s%60;
+                        }else {
+                            timeMsg = m + ":" + s%60;
+                        }
+                    }else{
+                        timeMsg = s + "s";
+                    }
+                }else {
+                    timeMsg = musicDetail.getMusic_time() + "ms";
+                }
+            }
+            if (musicDetail.getMusic_size() != null && musicDetail.getMusic_size() > 0 && musicDetail.getMusic_time() > 0 && musicDetail.getMusic_time() != null){
+                int bits = musicDetail.getMusic_size()/1024 * 8 / (musicDetail.getMusic_time()/1000);
+                bitMsg = bits + "kbps";
+            }
+            musicSize.setText(sizeMsg);
+            songLength.setText(timeMsg);
+            songBits.setText(bitMsg);
             musicPath.setText(musicDetail.getMusic_path());
 
         }
     }
-
 
 
     //设置状态栏颜色
